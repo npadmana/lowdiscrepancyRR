b6761b52514f3415c9039e1ca84e8b12fca9e57d
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-11-09 15:40:05 -0500

    More code updates....

--- mma/LowDiscrep.m
+++ mma/LowDiscrep.m
@@ -1,5 +1,12 @@
 (* Collection of code for generating low discrepancy sequences *)
 
+(* Reshape arrays *)
+Clear[reshape];
+reshape[ll_, {d__?((IntegerQ[#] && Positive[#]) &)}] := 
+  With[{e = Flatten[ll]}, 
+   Fold[Partition, e, 
+     Take[{d}, {-1, 2, -1}]] /; (Length[e] === Times[d])];
+
 (* Wrapper function for low discrepancy sequences in dimension d *)
 Clear[generateLDSGrid];
 generateLDSGrid[num_, dim_:2] := BlockRandom[
@@ -12,7 +19,7 @@ generateLDSGrid[num_, dim_:2] := BlockRandom[
 (* Generate a shifted low discrepancy sequence in dimension dim *)
 Clear[generateLDSshiftedGrid];
 generateLDSshiftedGrid[num_, dim_:2] := With[{x0 = RandomReal[{0, 1}, dim]}, 
-    Plus[x0, #] & /@  generateLDSGrid[num, dim] // FractionalPart
+    Plus[x0, #] & /@  generateLDSGrid[num, dim] // FractionalPart // reshape[#, {num, 2, dim/2}] &
    ];
    
 (* Generate a purely random grid *)   
@@ -32,3 +39,12 @@ getmeanerror[func_, nsamp_] := Module[{ll},
    {Mean[ll], StandardDeviation[ll]}
    ];
 SetAttributes[getmeanerror, HoldFirst];
+
+(* Basic Monte Carlo Integrator
+
+Applies a function to a list of points, and sums them, and divides by the numberof points
+ *)
+Clear[mcIntegrate];
+mcIntegrate[func_, ll_] := Total[func @@@ ll]/Dimensions[ll][[1]];
+SetAttributes[mcIntegrate, HoldFirst];
+
