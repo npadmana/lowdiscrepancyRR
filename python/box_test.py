781c0361114d8f22b631403388d6ef7b47cf2cba
John Parejko <john.parejko@yale.edu>
2012-11-09 15:19:52 -0500

    bulldogm changes to box_test

--- python/box_test.py
+++ python/box_test.py
@@ -3,7 +3,6 @@
 Generate random catalogs in a 0-1 box and computer their RR pair-counts.
 """
 import numpy as np
-from pairs import mr_wpairs
 import sys
 import subprocess
 
@@ -19,54 +18,52 @@ script = """#!/bin/bash
 
 cd astronomy/fastRR/
 
-mpirun -np NCPU python mr_2pc_bin.py -b distancebins_small_log2.dat -o OUT DATA
+mpirun -np NCPU python box_test_N.py -b distancebins_small_log2.dat --Nr NN -n nn
 """
 bashfilename = 'temp/NN_nn.sh'
 datafilename = 'temp/randoms_NN_nn.txt'
 
 def do_one(Nr,n):
     """Generate a random file with Nr points, labeled n, and compute RR on it."""
-    if Nr > 5e4:
+    if Nr < 5e4:
         NODES = 1
     else:
         NODES = 2
     NCPU = 8*NODES
     RAM = 40*NODES
-    points = np.random.random((3,Nr))
-    dataname = datafilename.replace('NN',str(Nr)).replace('nn',str(n))
-    np.savetxt(dataname)
 
     bashname = bashfilename.replace('NN',str(Nr)).replace('nn',str(n))
     bash = script.replace('NODES',str(NODES)).replace('RAM',str(RAM)).replace('NCPU',str(NCPU))
-    bash = bash.replace('OUT',bashname.replace('sh','dat')).replace('DATA',dataname)
     bash = bash.replace('NN',str(Nr)).replace('nn',str(n))
     outfile = open(bashname,'w')
     outfile.write(bash)
     outfile.close()
     print 'Started:',bashname
-    #subprocess.call('qsub '+bashname,shell=True)
+    subprocess.call('qsub '+bashname,shell=True)
 #...
 
 def main(argv=None):
     if argv is None: argv = sys.argv[1:]
     from optparse import OptionParser, OptionGroup
     
-    import pdb
-    pdb.set_trace()
     usage = '%prog [OPTION]'
+    usage += '\n\nGenerate (min,...,min*k^maxpow) random points and calculate RR.'
     parser = OptionParser(usage)
-    parser.add_option('-n',dest='n',type=int,default=3,
+    parser.add_option('-n',dest='n',type=int,default=10,
                       help='Number of random catalogs of each size to create (%default)')
-
+    parser.add_option('--min',dest='min',type=int,default=1000,
+                      help='Minimum number of randoms (%default)')
+    parser.add_option('--maxpow',dest='maxpow',type=int,default=5,
+                      help='Maximum power of k to multiply min by (%default)')
+    parser.add_option('--k',dest='k',type=int,default=2,
+                      help='Power to raise to when multiplying by min (%default)')
     (opts,args) = parser.parse_args(argv)
-    
-    Nrands = 1000*(2**np.arange(0,8,dtype=np.int))
+
+    Nrands = opts.min*(opts.k**np.arange(0,opts.maxpow,dtype=np.int))
 
     for Nr in Nrands:
         print 'Doing:',Nr
-        for n in range(opts.n):
-            print 'Number:',n
-            do_one(Nr,n)
+        do_one(Nr,opts.n)
 #...
 
 if __name__ == "__main__":
--- python/box_test_N.py
+++ python/box_test_N.py
@@ -5,6 +5,7 @@ Compute pair counts on N RR pairs.
 import numpy as np
 from pairs import mr_wpairs
 import sys
+import time
 
 from mpi4py import MPI
 
@@ -12,13 +13,10 @@ def do_counting(comm,rank,bins,data1,data2):
     """
     Count pairs in bins between data1 and data2.
     To count a dataset against itself, you must pass data.copy() as data2.
-    Counter determines which code is used for bin counting.
-    Specify weights to weight points by that amount.
-    Specify minboxwidth to tune the minimum size of each tree box.
     """
     t1 = time.time()
     print '%d of %d: Setting up...'%(comm.rank,comm.size)
-    wpairs = mr_wpairs.radial_wpairs(comm,data1,data2,w1=weight1,w2=weight2,minpart=200,minboxwidth=minboxwidth,ntrunc=1000)
+    wpairs = mr_wpairs.radial_wpairs(comm,data1,data2,minpart=200,ntrunc=1000)
     t2 = time.time()
     print '%d of %d: Setup time = %8.5f'%(comm.rank,comm.size,t2-t1)
     sys.stdout.flush()
@@ -49,7 +47,7 @@ def do_one(comm,rank,bins,Nr,outname):
     points2 = points.copy()
     if rank == 0:
         print '--------- Processing:',outname
-    result = do_counting(comm,rank,bins,points,points2)
+    counts = do_counting(comm,rank,bins,points,points2)
     if rank == 0:
         write_1d(bins,counts,outname)
 #...
@@ -64,7 +62,7 @@ def main(argv=None):
                       help='Number of random catalogs of each size to create (%default)')
     parser.add_option('--Nr',dest='Nr',type=int,default=1000,
                       help='Number of random points (%default)')
-    parser.add_option('-b','--bins',dest='binfile',default='distancebins.txt',
+    parser.add_option('-b','--bins',dest='binfile',default='distancebins_small_log2.dat',
                       help='file containing the bins to compute pairs in (%default)')
     (opts,args) = parser.parse_args(argv)
 
@@ -72,8 +70,8 @@ def main(argv=None):
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
-    outfilename = 'counts/NN_nn.dat'
-    
+    outfilename = '../data/NN_nn.dat'
+
     for n in range(opts.n):
         outname = outfilename.replace('NN',str(opts.Nr)).replace('nn',str(n))
         do_one(comm,rank,bins,opts.Nr,outname)
