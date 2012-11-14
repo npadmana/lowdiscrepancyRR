b6bd75e0a758d2e826c924b71ecc2ec63d2c1da4
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-11-14 17:31:04 -0500

    Define a toy mask.

--- cpp/ang2d/capintegrate.cpp
+++ cpp/ang2d/capintegrate.cpp
@@ -5,5 +5,18 @@
 
 #include "ang2d.h"
 
+/* Simple mask, centered on the N-pole at dec > dec0 */
+class Cap {
+public :
+	double dec0;
+	Cap(double dec0_) : dec0(dec0_) {};
 
+	double operator()(double ra, double dec);
+
+};
+
+double Cap::operator()(double ra, double dec) {
+	if (dec > dec0) {return 1.0;};
+	return 0.0;
+}
 
