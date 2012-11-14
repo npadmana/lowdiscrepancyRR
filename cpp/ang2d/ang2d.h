082df41c18e4e2e508e56c1d587afbe0e86ee9d7
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-11-14 17:21:32 -0500

    Framework for doing angular RR integrals.

---
+++ cpp/ang2d/ang2d.h
@@ -0,0 +1,145 @@
+/** RR calculation using quasi-Monte Carlo methods.
+ *
+ * Nikhil Padmanabhan, Yale
+ * November 13, 2012
+ */
+
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <utility>
+#include <Eigen/Core>
+#include "src/npgsl/npRandom.h"
+
+namespace Ang2D {
+
+using namespace std;
+using namespace Eigen;
+
+
+const int DIM = 4;
+const double PI = 3.1415926535897932385;
+const double D2R = 0.017453292519943295769; // Degrees to radians
+
+typedef pair<double, double> dpair;
+
+
+/** Compute the angular RR integral
+ *
+ *	IMPORTANT NOTE : The code assumes RA runs from [0,360], and Dec from [-90,90]
+ *
+ *	The bounds (except for RAbounds, see below) are assumed to be increasing. If not, an exception is thrown.
+ *  For the RABounds, if the second elt is lower, we assume we have wrapped around the sphere.
+ *
+ *  The last element of thetaBin cannot be > 180.0, and the first element must be >= 0.
+ *
+ *   @param RABounds (pair<double, double>)  -- RA bounds to consider, should correspond normally to mask 1 (degrees)
+ *   @param DecBounds (pair<double, double>) -- DecBounds to consider (degrees)
+ *   @param thetaBin (pair<double, double>) -- min, max of the bin (degrees)
+ *	 @param nrand (int) -- number of randoms
+ *	 @param nsim (int) -- number of simulations
+ *	 @param Mask1 (templated class) -- class where operator() takes RA, Dec (in degrees) and returns a double (weight)
+ *	 @param Mask2 (templated class) -- class where operator() takes RA, Dec (in degrees) and returns a double (weight)
+ *
+ *	 Returns vector<double> rr[nsim] --- nsim measurements of RR
+ */
+template <class Mask>
+vector<double> rreval(dpair RABounds, dpair DecBounds, dpair thetaBin, int nrand, int nsim, Mask &Mask1, Mask &Mask2) {
+	// Dec data validity
+	if ((DecBounds.first < -90) || (DecBounds.first > 90)) throw ("RABounds exception thrown");
+	if ((DecBounds.second < -90) || (DecBounds.second > 90)) throw ("RABounds exception thrown");
+	if (DecBounds.second < DecBounds.first) throw("Dec Bounds exception thrown");
+
+	// theta data validity
+	if (thetaBin.second < thetaBin.first) throw("thetaBin exception thrown");
+	if ((thetaBin.first < 0) || (thetaBin.first > 180)) throw("thetaBin exception thrown");
+	if ((thetaBin.second < 0) || (thetaBin.second > 180)) throw("thetaBin exception thrown");
+
+	// RA
+	if ((RABounds.first < 0) || (RABounds.first > 360.0)) throw ("RABounds exception thrown");
+	if ((RABounds.second < 0) || (RABounds.second > 360.0)) throw ("RABounds exception thrown");
+	if (RABounds.second < RABounds.first) RABounds.second += 360; // Wrap around
+
+
+	// Survey Bounds
+	double cth1 = sin(DecBounds.first * D2R); // Cos theta_1
+	double dcth = sin(DecBounds.second*D2R) - cth1; // Cos theta_2 - Cos theta_1
+	double phi1 = RABounds.first * D2R; // phi_1
+	double dphi = RABounds.second * D2R; // phi_2 - phi1
+
+	// Scatter bounds
+	double cDth1 = cos(thetaBin.second * D2R); // Cos Delta theta_1 --- use the larger number first, to avoid -ve's in the code
+	double dcDth = cos(thetaBin.first * D2R) - cDth1; // Cos Delta theta_2 - Cos Delta theta_1
+
+	// Jacobian
+	double jacobian = dcth * dphi * dcDth * 2 * PI;
+
+	// Set up the output vector
+	vector<double> outvec(nsim);
+
+	// Set up the random vector
+	vector<double> x0(DIM);
+	npRandom rng(1234);
+
+	// Temporary values
+	double ra1, dec1, ra2, dec2, out, tmp, cth_, sth_, phi_;
+	Vector3d x1, x2; Matrix3d rotmat;
+
+	for(int jj=0; jj<nsim; ++jj) {
+
+		for_each(x0.begin(), x0.end(), [&rng](double &x){x=rng();});
+
+		// Set up the quasi RNG
+		npQuasiRandom qrng(dim);
+
+		// Initialize the integrator
+		out=0.0;
+
+		// Loop over points
+		for (int ii=0; ii<nrand; ++ii) {
+
+			// Generate a random pseudo-random number and shift it mod 1
+			vector<double> x = qrng();
+			transform(x0.begin(), x0.end(), x.begin(), x.begin(),
+					[&r](double a, double b){return modf(a+b,&tmp);});
+
+			// Work out the RA, Dec of position 1
+			x[1] = ((x[1] * dphi) + phi1 );
+			x[0] = x[0]*dcth + cth1;
+			ra1 = x[1]/D2R; if (ra1 > 360) ra1 = ra1-360;
+ 			dec1 = 90 - acos(x[1])/D2R;
+
+			// Work out displacement vector
+			phi_ = x[3]*2 *pi;
+			cth_ = x[2]*dcDth + cDth1;
+			sth_ = sqrt(1-cth_*cth_);
+			x1 << sth_ * cos(phi_), sth_ * sin(phi_), cth_;
+
+			// Generate rotation matrix
+			tmp = sqrt(1-x[0]*x[0]); // sin
+			// x[0] = cos theta
+			// tmp = sin theta
+			// x[1] = phi
+			rotmat << x[0] * cos(x[1]), -sin(x[1]), tmp *cos(x[1]),
+					  x[0] * sin(x[1]),  cos(x[1]), x[0]*sin(x[1]),
+					  tmp             ,          0, x[0];
+
+			x2.noalias() = rotmat * x1;
+
+			// Now convert back to ra, dec
+			dec2 = 90 - acos(x2.coeff(2))/D2R; // no range checking
+			ra2 = atan2(x2.coeff(1), x2.coeff(0))/D2R; // (-180, 180)
+			if (ra2 < 0) ra2 = 360+ra2;
+
+			// Now do the actual calculation
+			out += Mask1(ra1, dec1) * Mask2(ra2, dec2);
+
+		}
+
+		// Multiply back in the jacobian
+		outvec[jj]  = out/(nrand) * jacobian;
+	}
+	return outvec;
+}
+
+}
