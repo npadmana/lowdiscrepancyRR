b447a0d4cd67250a7ca2b12cfc2c173e8ba24735
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-12-07 21:49:29 -0500

    Lots of changes

--- mma/plots.m
+++ mma/plots.m
@@ -1,5 +1,10 @@
 (* Codes for plots... *)
 
+(* Some shapes etc *)
+pSquare = Graphics[{Rectangle[]}];
+pDisk = Graphics[{Disk[]}];
+
+
 (* Code to read in the binary files we generate *)
 
 readSims[fn_] := Module[{hdr, ff, out}, 
@@ -7,7 +12,7 @@ readSims[fn_] := Module[{hdr, ff, out},
 	hdr = BinaryRead[ff, {"Integer32", "Integer32", "Real64", "Real64"}];
 	out = BinaryReadList[ff, "Real64", hdr[[1]]];
 	Close[ff];
-	out
+	{hdr, out}
 ];
 
 stats[ll_List] := {Mean[ll], StandardDeviation[ll], StandardDeviation[ll]/Mean[ll]};
@@ -15,4 +20,4 @@ stats[ll_List] := {Mean[ll], StandardDeviation[ll], StandardDeviation[ll]/Mean[l
 genfilenames[hdr_, ibin_, npts_] := hdr <> "_" <> IntegerString[ibin, 10, 2]  \
 	<> "_" <> IntegerString[npts, 10, 8] <> ".dat";
 
-processbin[hdr_, ibin_, nptlist_] := {#,stats[readSims[genfilenames[hdr, ibin, #]]]} & /@ nptlist;
+processbin[hdr_, ibin_, nptlist_] := With[{tmp=readSims[genfilenames[hdr, ibin, #]]}, {tmp[[1]],stats[tmp[[2]]]}] & /@ nptlist;
