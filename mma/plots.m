bbb067c8763054e8d923a8736cca0e635e144c9d
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-12-08 01:32:02 -0500

    Improvements to plot

--- mma/plots.m
+++ mma/plots.m
@@ -4,16 +4,32 @@
 pSquare = Graphics[{Rectangle[]}];
 pDisk = Graphics[{Disk[]}];
 
+Clear[npLogTicks];
+npLogTicks[lnstart_, lnend_, label_:True] :=
+    Module[ {bigticks, smallticks},
+        smallticks = 
+         Table[{10.0^i*j, "", {0.01, 0}}, {i, lnstart, lnend}, {j, 2., 
+            9}] // Flatten[#, {{1, 2}, {3}}] &;
+        bigticks = 
+         Table[{10.0^i, If[ label,
+                            Superscript[10, i],
+                            ""
+                        ], {0.02, 0}}, {i, 
+           lnstart, lnend}];
+        Join[bigticks, smallticks]
+    ];
+
 
 (* Code to read in the binary files we generate *)
 
-readSims[fn_] := Module[{hdr, ff, out}, 
-	ff = OpenRead[fn, BinaryFormat->True];
-	hdr = BinaryRead[ff, {"Integer32", "Integer32", "Real64", "Real64"}];
-	out = BinaryReadList[ff, "Real64", hdr[[1]]];
-	Close[ff];
-	{hdr, out}
-];
+readSims[fn_] :=
+    Module[ {hdr, ff, out},
+        ff = OpenRead[fn, BinaryFormat->True];
+        hdr = BinaryRead[ff, {"Integer32", "Integer32", "Real64", "Real64"}];
+        out = BinaryReadList[ff, "Real64", hdr[[1]]];
+        Close[ff];
+        {hdr, out}
+    ];
 
 stats[ll_List] := {Mean[ll], StandardDeviation[ll], StandardDeviation[ll]/Mean[ll]};
 
