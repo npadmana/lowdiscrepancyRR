f9b38ae65d7934ace0cb1fd0ed31511e87611263
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-12-08 11:19:49 -0500

    Refactor some of the plotting code slightly to pull out common pieces.

--- mma/plots.m
+++ mma/plots.m
@@ -1,24 +1,6 @@
 (* Codes for plots... *)
 
-(* Some shapes etc *)
-pSquare = Graphics[{Rectangle[]}];
-pDisk = Graphics[{Disk[]}];
-
-Clear[npLogTicks];
-npLogTicks[lnstart_, lnend_, label_:True] :=
-    Module[ {bigticks, smallticks},
-        smallticks = 
-         Table[{10.0^i*j, "", {0.01, 0}}, {i, lnstart, lnend}, {j, 2., 
-            9}] // Flatten[#, {{1, 2}, {3}}] &;
-        bigticks = 
-         Table[{10.0^i, If[ label,
-                            Superscript[10, i],
-                            ""
-                        ], {0.02, 0}}, {i, 
-           lnstart, lnend}];
-        Join[bigticks, smallticks]
-    ];
-
+Needs["Misc`npPlotting`"];
 
 (* Code to read in the binary files we generate *)
 
@@ -37,3 +19,22 @@ genfilenames[hdr_, ibin_, npts_] := hdr <> "_" <> IntegerString[ibin, 10, 2]  \
 	<> "_" <> IntegerString[npts, 10, 8] <> ".dat";
 
 processbin[hdr_, ibin_, nptlist_] := With[{tmp=readSims[genfilenames[hdr, ibin, #]]}, {tmp[[1]],stats[tmp[[2]]]}] & /@ nptlist;
+
+(* Read in John's RR counts 
+
+Three functions :
+  readjp : reads in one file and dumps out a normalized array
+  readalljp : reads in all the files
+  processjp : data in a similar format as previously.
+*)
+
+readjp[fn_] := Module[{dat, norm}, 
+	dat = Import[fn,"Table"];
+	norm = dat[[1,1]];
+	dat[[3;;,3]]/norm^2
+]
+
+readalljp[dir_, npts_] := Module[{fnlist},
+	fnlist = FileNames[dir<>IntegerString[npts]<>"*.dat"];
+	readjp /@ fnlist
+];
