dd907f6d6bdc3275dc0cde0481add740c1f96bdc
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-12-28 16:08:43 -0500

    test something.

--- cpp/boss/test_program_options.cpp
+++ cpp/boss/test_program_options.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <boost/program_options.hpp>
 
 using namespace std;
@@ -7,16 +8,24 @@ namespace po = boost::program_options;
 
 int main(int argc, char** argv) {
 	vector<double> foo;
+	string fn;
 	{
 		try {
 			po::options_description desc("Allowed options");
 			desc.add_options()
 	    				("doubles", po::value< vector<double> >(&foo)->multitoken(), "doubles")
+	    				("fn", po::value<string>(&fn)->default_value(string()),"fn");
 	    				;
 			po::variables_map vm;
 			po::store(po::parse_command_line(argc, argv, desc), vm);
 			po::notify(vm);
 
+			if (vm.count("fn")) {
+				cout << "fn option found" << endl;
+			}
+			if (fn.empty()) {
+				cout << "String is empty" << endl;
+			}
 
 		}
 		catch (exception &e) {
