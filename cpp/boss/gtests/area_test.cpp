af5254fd082e91e27148a9463538b4d32ae3f40f
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2012-12-30 18:15:23 -0500

    Detailed area tests for hemisphere and sphere done and passed.
    OPEN - task 26: Area code needs unit test 
    http://github.com/npadmana/lowdiscrepancyRR/issues/issue/26

--- cpp/boss/gtests/area_test.cpp
+++ cpp/boss/gtests/area_test.cpp
@@ -1,4 +1,7 @@
 #include "gtest/gtest.h"
+
+#include <string>
+
 #include "../ang2d.h"
 #include "../BossMask.h"
 
@@ -7,31 +10,37 @@
 using namespace std;
 using namespace Boss;
 
-class HemisphereAreaTest : public ::testing::TestWithParam<int> {
-	// Empty test body
+class AreaTest : public ::testing::TestWithParam<int> {
+public:
+	double calcArea(string fn, int nproc) {
+		Ang2D::InputParams p0(fn);
+		// Now define the mask
+		BossMask mask1(p0.mask1fn);
+		// Define various bounds
+		int nra, ndec;
+		tie(ndec, nra) = Ang2D::partition(nproc);
+
+		double areasum = 0.0;
+		for (int irank=0; irank < nproc; ++irank) {
+			Ang2D::setBounds(nra,ndec,irank,mask1,p0);
+			Ang2D::OutputData out = Ang2D::area(mask1, p0);
+			out.finalize();
+			areasum += out.mean();
+		}
+		return areasum;
+	}
 };
 
-TEST_P(HemisphereAreaTest, MultiSection) {
-	int nproc = GetParam();
-	Ang2D::InputParams p0("hemisphere_input.cfg");
-	// Now define the mask
-	BossMask mask1(p0.mask1fn);
-	// Define various bounds
-	int nra, ndec;
-	tie(ndec, nra) = Ang2D::partition(nproc);
-
-	double areasum = 0.0;
-	for (int irank=0; irank < nproc; ++irank) {
-		Ang2D::setBounds(nra,ndec,irank,mask1,p0);
-		Ang2D::OutputData out = Ang2D::area(mask1, p0);
-		out.finalize();
-		areasum += out.mean();
-	}
-	EXPECT_NEAR(1.0, areasum, 1.e-3);
+TEST_P(AreaTest, Hemisphere) {
+	EXPECT_NEAR(1.0, calcArea("hemisphere_input.cfg", GetParam()), 1.e-3);
 }
 
-INSTANTIATE_TEST_CASE_P(HemisphereAreas,
-                        HemisphereAreaTest,
+TEST_P(AreaTest, Sphere) {
+	EXPECT_NEAR(1.0, calcArea("sphere_input.cfg", GetParam()), 1.e-3);
+}
+
+INSTANTIATE_TEST_CASE_P(MultiSectionAreaTest,
+                        AreaTest,
                         ::testing::Values(1,2,4,8,16,32));
 
 
