6dce5825230690dec8de9cb35ee20324a5626ac0
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-01-09 16:27:52 -0500

    New log spacing

---
+++ data/boss/lds_lowz_angular/calc_log_RR.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -q long
+#PBS -l nodes=8:ppn=8
+#PBS -l mem=256gb
+#PBS -l walltime=5:00:00
+#PBS -j oe
+#PBS -o calc_log_RR.out
+
+cd $PBS_O_WORKDIR
+ulimit -l unlimited
+mpirun -np 64 ../../../cpp/build/boss/bossRR lowz_log_input.cfg 
+
